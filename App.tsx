6467bbf7e9701144140ca325b34c4beba42dda8b
Karen Vicente <karenyasmin37@gmail.com>
2022-08-11 22:15:14 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
+import "react-native-gesture-handler";
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+import theme from "./src/global/styles/theme";
+import { Routes } from "./src/routes";
+import { AppRoutes } from "./src/routes/app.routes";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SignIn } from "./src/screens/Signin";
+
+import { AuthProvider, useAuth } from "./src/hooks/auth";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  const { userStorageLoading } = useAuth();
+
+  if (!fontsLoaded || userStorageLoading) {
+    return <AppLoading />;
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <StatusBar barStyle="light-content" />
+
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
+  );
+}
